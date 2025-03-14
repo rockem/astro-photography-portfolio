18d43b45a71347e0cc778196b301d121dfd1de8a
eli segal <eli.segal@dynamicyield.com>
2025-03-14 23:36:22 +0200

    Added footer

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import {Github, Instagram} from "lucide-astro";
+
+export const siteConfig = {
+  title: "Eli Segal's Photography",
+  owner: "Eli Segal",
+  socialLinks: [
+    {name: "GitHub", url: "https://github.com/rockem", icon: Github},
+    {name: "Instagram", url: "https://www.instagram.com/lesegal/", icon: Instagram},
+  ]
+}
