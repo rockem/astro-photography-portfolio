28d1f1cad64878a476f75a7f35ecfdddb6a14399
eli segal <eli.segal@dynamicyield.com>
2025-03-20 21:44:10 +0200

    Added pre-commit with prettier

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,14 @@
-import {Github, Instagram} from "lucide-astro";
+import { Github, Instagram } from "lucide-astro";
 
 export const siteConfig = {
   title: "Eli Segal's Photography",
   owner: "Eli Segal",
   socialLinks: [
-    {name: "GitHub", url: "https://github.com/rockem", icon: Github},
-    {name: "Instagram", url: "https://www.instagram.com/lesegal/", icon: Instagram},
-  ]
-}
+    { name: "GitHub", url: "https://github.com/rockem", icon: Github },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/lesegal/",
+      icon: Instagram,
+    },
+  ],
+};
