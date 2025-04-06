ad4362ddc0cb2e65a10f69c2e79fe48df2157fbc
Eli Segal <eli.segal@gmail.com>
2025-04-06 14:30:11 +0300

    Load gallery.yaml from src

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rockem.github.io',
 	base: 'astro-photography-portfolio',
 	vite: {
-		plugins: [
-			tailwindcss(),
-			viteStaticCopy({
-				targets: [
-					{
-						src: 'src/gallery/gallery.yaml',
-						dest: 'gallery',
-					},
-				],
-			}),
-		],
+		plugins: [tailwindcss()],
 	},
 });
