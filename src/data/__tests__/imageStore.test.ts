a3146202c10ba7ae4b32860c7010a335abdaaf89
Eli Segal <eli.segal@gmail.com>
2025-04-15 00:49:21 +0300

    refactoring

--- src/data/__tests__/imageStore.test.ts
+++ src/data/__tests__/imageStore.test.ts
@@ -7,26 +7,24 @@ const { getImages } = await import('../imageStore.ts');
 
 describe('Images Store', () => {
 	test('should retrieve all present images', async () => {
-		const imagesData = await getImages({}, 'src/data/__tests__/gallery');
+		const imagesData = await getTestImages();
 		expect(imagesData).toHaveLength(2);
 		expect(imagesData[0].src).toEqual(kukuTrees);
 		expect(imagesData[1].src).toEqual(popoView);
 	});
 
+	async function getTestImages(filter: any = {}) {
+		return await getImages(filter, 'src/data/__tests__/gallery');
+	}
+
 	test('should retrieve only featured images', async () => {
-		const images = await getImages(
-			{ featured: true },
-			'src/data/__tests__/gallery',
-		);
+		const images = await getTestImages({ featured: true });
 		expect(images).toHaveLength(1);
 		expect(images[0].alt).toEqual('Popo View');
 	});
 
 	test('should retrieve images of specific collection', async () => {
-		const images = await getImages(
-			{ collection: 'popo' },
-			'src/data/__tests__/gallery',
-		);
+		const images = await getTestImages({ collection: 'popo' });
 		expect(images).toHaveLength(1);
 		expect(images[0].description).toContain('popo album');
 	});
