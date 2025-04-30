54cb089758c1b08bffce27f7a9a0b3052be34dff
eli segal <eli.segal@dynamicyield.com>
2025-04-30 15:10:05 +0300

    Implemented write of collection ids

--- src/data/gallery-generator.ts
+++ src/data/gallery-generator.ts
@@ -2,24 +2,49 @@ import { program } from 'commander';
 import * as fs from 'node:fs';
 import yaml from 'js-yaml';
 import path from 'path';
+import fg from 'fast-glob';
 
-export const defaultGalleryFileName = 'gallery.yaml';
+const defaultGalleryFileName = 'gallery.yaml';
 
-async function createGalleryFile(directoryPath: string): Promise<void> {
+async function createGalleryFile(galleryDir: string): Promise<void> {
 	try {
-		const galleryObj = {
-			collections: [{ id: 'kuku' }, { id: 'popo' }],
-		};
-		const filePath = path.join(directoryPath, defaultGalleryFileName);
-		await fs.promises.writeFile(filePath, yaml.dump(galleryObj), 'utf8');
-		console.log('Gallery file created successfully at:', filePath);
+		const galleryObj = await createGalleryObjFrom(galleryDir);
+		await writeGalleryYaml(galleryDir, galleryObj);
 	} catch (error) {
 		console.error('Failed to create gallery file:', error);
 		process.exit(1);
 	}
 }
 
-program.argument('<path to gallery images>');
+async function createGalleryObjFrom(galleryDir: string) {
+	const imageFiles = await fg(`${galleryDir}/**/*.{jpg,jpeg,png}`, {
+		dot: false,
+	});
+	const galleryObj = {
+		collections: createCollectionsFrom(imageFiles, galleryDir),
+	};
+	return galleryObj;
+}
+
+function createCollectionsFrom(imageFiles: string[], galleryDir: string) {
+	return imageFiles.map((file) => {
+		const relativePath = path.relative(galleryDir, file); // e.g. "collection1/image.jpg"
+		return {
+			id: path.dirname(relativePath),
+		}; // e.g. "collection1"
+	});
+}
+
+async function writeGalleryYaml(
+	galleryDir: string,
+	galleryObj: { collections: { id: string }[] },
+) {
+	const filePath = path.join(galleryDir, defaultGalleryFileName);
+	await fs.promises.writeFile(filePath, yaml.dump(galleryObj), 'utf8');
+	console.log('Gallery file created successfully at:', filePath);
+}
+
+program.argument('<path to images directory>');
 program.parse();
 
 const directoryPath = program.args[0];
