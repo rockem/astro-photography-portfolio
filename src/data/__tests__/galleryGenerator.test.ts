b30a7cfed7fd73677798d56b1e53e2ea9a2d2f95
eli segal <eli.segal@dynamicyield.com>
2025-05-01 08:13:12 +0300

    Tests refactoring

--- src/data/__tests__/galleryGenerator.test.ts
+++ src/data/__tests__/galleryGenerator.test.ts
@@ -3,11 +3,19 @@ import { execa } from 'execa';
 import path from 'path';
 import { promises as fs } from 'fs';
 import * as yaml from 'js-yaml';
-import type { Collection, GalleryData } from '../gallerySchema.ts';
+import type { GalleryData } from '../gallerySchema.ts';
 
 const testGalleryPath = 'src/data/__tests__/gallery';
 const testGalleryYaml = path.join('src/data/__tests__/gallery', 'gallery.yaml');
 const scriptPath = path.resolve(__dirname, '../gallery-generator.ts');
+
+function expectContainsOnlyObjectsWith(objArray: unknown, partials: unknown[]) {
+	expect(objArray).toHaveLength(partials.length);
+	console.log('Partials:', partials);
+	expect(objArray).toEqual(
+		expect.arrayContaining(partials.map((p) => expect.objectContaining(p))),
+	);
+}
 
 describe('Test Gallery Generator', () => {
 	let gallery: GalleryData;
@@ -19,11 +27,10 @@ describe('Test Gallery Generator', () => {
 	});
 
 	it('should add directories as collections', async () => {
-		const collectionIds = gallery.collections.map((col: Collection) => col.id);
-
-		expect(collectionIds).toHaveLength(2);
-		expect(collectionIds).toContain('kuku');
-		expect(collectionIds).toContain('popo');
+		expectContainsOnlyObjectsWith(gallery.collections, [
+			{ id: 'kuku' },
+			{ id: 'popo' },
+		]);
 	});
 
 	async function loadGalleryDataFrom(galleryPath: string) {
@@ -32,35 +39,24 @@ describe('Test Gallery Generator', () => {
 		return yaml.load(content) as GalleryData;
 	}
 
-	it('should add collection names', async () => {
-		const collectionNames = gallery.collections.map(
-			(col: Collection) => col.name,
-		);
-
-		expect(collectionNames).toHaveLength(2);
-		expect(collectionNames).toContain('Kuku');
-		expect(collectionNames).toContain('Popo');
+	it('should add collection camel case names', async () => {
+		expectContainsOnlyObjectsWith(gallery.collections, [
+			{ name: 'Kuku' },
+			{ name: 'Popo' },
+		]);
 	});
 
 	it('should add images path', async () => {
-		const images = gallery.images;
-
-		const imagesPath = images.map((img) => img.path);
-
-		expect(imagesPath).toHaveLength(2);
-		expect(imagesPath).toContain('kuku/kuku-trees.jpg');
-		expect(imagesPath).toContain('popo/popo-view.jpg');
+		expectContainsOnlyObjectsWith(gallery.images, [
+			{ path: 'kuku/kuku-trees.jpg' },
+			{ path: 'popo/popo-view.jpg' },
+		]);
 	});
 
 	it('should add images name and description', async () => {
-		const images = gallery.images;
-
-		expect(images).toHaveLength(2);
-		expect(images).toEqual(
-			expect.arrayContaining([
-				expect.objectContaining({ title: 'Kuku Trees', description: '' }),
-				expect.objectContaining({ title: 'Popo View', description: '' }),
-			]),
-		);
+		expectContainsOnlyObjectsWith(gallery.images, [
+			{ title: 'Kuku Trees', description: '' },
+			{ title: 'Popo View', description: '' },
+		]);
 	});
 });
