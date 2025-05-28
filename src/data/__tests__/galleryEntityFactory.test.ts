08a5c1fc3640e25155ec776c870809bfebc17056
eli segal <eli.segal@dynamicyield.com>
2025-05-28 16:30:09 +0300

    more PR fixes

--- src/data/__tests__/galleryEntityFactory.test.ts
+++ src/data/__tests__/galleryEntityFactory.test.ts
@@ -10,13 +10,13 @@ describe('test create gallery images', () => {
 			testGalleryPath,
 			path.join(testGalleryPath, 'kuku', 'kuku-trees.jpg'),
 		);
-		expect(image.exif.captureDate).toEqual(new Date('2025-02-21T09:17:14.000Z'));
-		expect(image.exif.fNumber).toEqual(8);
-		expect(image.exif.focalLength).toEqual(28);
-		expect(image.exif.iso).toEqual(100);
-		expect(image.exif.shutterSpeed).toEqual(640);
-		expect(image.exif.model).toEqual('LEICA Q3');
-		expect(image.exif.lensModel).toEqual('SUMMILUX 1:1.7/28 ASPH.');
+		expect(image.exif?.captureDate).toEqual(new Date('2025-02-21T09:17:14.000Z'));
+		expect(image.exif?.fNumber).toEqual(8);
+		expect(image.exif?.focalLength).toEqual(28);
+		expect(image.exif?.iso).toEqual(100);
+		expect(image.exif?.shutterSpeed).toEqual(640);
+		expect(image.exif?.model).toEqual('LEICA Q3');
+		expect(image.exif?.lensModel).toEqual('SUMMILUX 1:1.7/28 ASPH.');
 	});
 
 	it('should retrieve image without exif data', async () => {
